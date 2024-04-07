38cc4d61af2aadb6c6660d4af4be5fc4ea03995c
alexquar <alexanderquarrie@gmail.com>
2024-04-07 10:42:53 -0400

    signup function

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,12 +4,16 @@ export default function Signup() {
   const [password, setPassword]= useState('')
   const [email,setEmail]= useState('')
   const [displayName, setDisplayName]= useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="container text-center py-5 my-5">
     <div className="row justify-content-center">
     <div class="card col-12 col-sm-10 col-lg-6">
     <div class="card-body">
-      <form className="d-flex flex-column" >
+      <form onSubmit={(e)=>handleSubmit(e)} className="d-flex flex-column" >
         <h2>
           Sign Up
         </h2>
@@ -17,19 +21,22 @@ export default function Signup() {
     <span className="card-text mx-2">
     Email:
     </span>
-    <input type="email" />
+    <input type="email"   onChange={(e)=>setEmail(e.target.value)}
+    value={email} />
       </label>
       <label className="my-3" >
     <span className="mx-2 card-text">
     Password:
     </span>
-    <input type="password" />
+    <input type="password"   onChange={(e)=>setPassword(e.target.value)}
+    value={password}/>
       </label>
       <label className="my-3">
     <span className="mx-2 card-text">
         Display Name:
     </span>
-    <input type="text" />
+    <input   onChange={(e)=>setDisplayName(e.target.value)}
+    value={displayName} type="text" />
       </label>
       <div className="d-grid mt-3 col-4 mx-auto">
       <button className="btn btn-dark">Submit</button>
