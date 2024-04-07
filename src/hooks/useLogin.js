b06f9293c2e5f47a00e8f25f5d6d73b5ba451eaa
alexquar <alexanderquarrie@gmail.com>
2024-04-07 10:36:17 -0400

    useLogin

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import { projectAuth } from '../firebase/config'
+import { useAuthContext } from './useAuthContext'
+
+export const useLogin = () => {
+  const [isCancelled, setIsCancelled] = useState(false)
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const { dispatch } = useAuthContext()
+
+  const login = async (email, password) => {
+    setError(null)
+    setIsPending(true)
+  
+    try {
+      // login
+      const res = await projectAuth.signInWithEmailAndPassword(email, password)
+
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user })
+
+        setIsPending(false)
+        setError(null)
+    } 
+    catch(err) {
+        setError("Incorrect password or email...")
+        setIsPending(false)
+    }
+  }
+
+  useEffect(() => {
+    return () => setIsCancelled(true)
+  }, [])
+
+  return { login, isPending, error }
+}
