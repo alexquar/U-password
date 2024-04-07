e811a66cd93dc20870cf9d0214a8af824c35c7ea
alexquar <alexanderquarrie@gmail.com>
2024-04-07 18:48:32 -0400

    check password page

--- src/pages/CheckPassword.jsx
+++ src/pages/CheckPassword.jsx
@@ -13,15 +13,34 @@ export default function CheckPassword({option}) {
         if (password.match(/[0-9]+/)){
             strength+=1;
         }
-        if (password.match(/[$@#&!*^.'"|-_=+%,<>;;`~?]+/)){
+        if (password.match(/[$@#&!*^.%,<>;;`~?]+/)){
             strength+=1;
     
             }
         setStrengthBar(`${strength}`)
     }
   return (
-    <div>
-      
+    <div className="container text-center my-5 py-5">
+<div className="card text-center">
+  <div className="card-header py-5">
+    Test Password Strength Here
+  </div>
+  <div className="card-body py-5">
+    <h5 className="card-title">Special title treatment</h5>
+    <form onSubmit={checkPassword} className="row m-3 justify-content-center">
+  <label htmlFor="colFormLabel" className="col-6 col-md-3 col-form-label">Password:</label>
+  <div className="col-6">
+    <input type="text" className="form-control" id="colFormLabel" placeholder="Password Here!"/>
+  </div>
+  <div className="col-3 mt-3 mt-md-0">
+  <button className="btn btn-secondary px-4">Test</button>
+  </div>
+</form>
+  </div>
+  <div className="card-footer py-5 text-muted">
+    2 days ago
+  </div>
+</div>
     </div>
   )
 }
