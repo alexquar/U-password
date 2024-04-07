1a1baf1ef1c0cc2132363a2da8f3311fec4d9c27
alexquar <alexanderquarrie@gmail.com>
2024-04-07 15:52:49 -0400

    card grid

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,9 +1,15 @@
+import PasswordCard from "./PasswordCard";
 
-
-export default function CardGrid() {
+export default function CardGrid(passwords) {
   return (
-    <div>
-      
+    <div className="container text-center gx-5 gy-3">
+        <div className="row justify-content-center">
+    {passwords && passwords.map(password => (
+        <div className="col-12 col-md-6 col-lg-4">
+       <PasswordCard password={password.password} createdAt={password.createdAt} service={password.service}/>
+       </div>
+    ))}
+    </div>
     </div>
   )
 }
