7436c5b1f50f1c56f3496ad1253dfae6b5db1348
alexquar <alexanderquarrie@gmail.com>
2024-04-07 17:21:21 -0400

    improving how passwords are shown

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -2,8 +2,8 @@ import PasswordCard from "./PasswordCard";
 
 export default function CardGrid({passwords}) {
   return (
-    <div className="container text-center gx-5 gy-3">
-        <div className="row justify-content-center">
+    <div className="container text-center gx-5">
+        <div className="row justify-content-start">
     {passwords && passwords.map(password => (
         <div className="col-12 col-md-6 col-lg-4">
        <PasswordCard key={password.id} password={password}/>
--- src/components/PasswordCard.jsx
+++ src/components/PasswordCard.jsx
@@ -11,13 +11,15 @@ export default function PasswordCard({password : data}) {
         }
     }
   return (
-    <div className="card">
+    <div className="card text-bg-dark my-3">
     <div className="card-body">
       <h5 className="card-title">Password for {service}</h5>
-    {createdAt &&  <p className="cart-text">Password created/changed: {createdAt.seconds}</p>}
-    {!showPassword &&  <button className="btn" onClick={handleClick}>Show Password</button>}
-    {showPassword &&  <button className="btn" onClick={handleClick}>Show Password</button>}
-    {showPassword && <p className="card-text">Password: {password}</p>}
+    {createdAt &&  <p className="cart-text">Password created/changed: {createdAt.toDate().toDateString()}</p>}
+    {!showPassword &&  <button className="btn btn-outline-light" onClick={handleClick}>Show Password</button>}
+    {showPassword &&  <button className="btn btn-outline-light" onClick={handleClick}>Show Password</button>}
+    <div>
+    {showPassword && <p className=" block badge rounded-pill text-bg-light mt-3 mb-0 p-3">Password: {password}</p>}
+    </div>
     </div>
   </div>
   )
