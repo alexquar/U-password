e4733d67b1d8af8726fa0d653200491a2395b661
alexquar <alexanderquarrie@gmail.com>
2024-04-07 19:04:24 -0400

    progress bar

--- src/pages/CheckPassword.jsx
+++ src/pages/CheckPassword.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 export default function CheckPassword({option}) {
     const [password,setPassword] = useState(option)
-    const [strengthBar, setStrengthBar]= useState('')
+    const [strengthBar, setStrengthBar]= useState('0')
     const checkPassword =()=>{
         let strength=0;
         if (password.match(/[a-z]+/)){
@@ -15,8 +15,16 @@ export default function CheckPassword({option}) {
         }
         if (password.match(/[$@#&!*^.%,<>;;`~?]+/)){
             strength+=1;
-    
+            
             }
+        if (password.length()>6){
+                strength+=1;
+                
+         }
+        if (password.length()>12){
+                    strength+=1;
+                    
+        }
         setStrengthBar(`${strength}`)
     }
   return (
@@ -26,8 +34,8 @@ export default function CheckPassword({option}) {
     Test Password Strength Here
   </div>
   <div className="card-body py-5">
-    <h5 className="card-title">Special title treatment</h5>
-    <form onSubmit={checkPassword} className="row m-3 justify-content-center">
+    <h5 className="card-title">The strength of your password with be reflected in the bar as the bottom!</h5>
+    <form onSubmit={checkPassword} className="row m-3 pt-3 justify-content-center">
   <label htmlFor="colFormLabel" className="col-6 col-md-3 col-form-label">Password:</label>
   <div className="col-6">
     <input type="text" className="form-control" id="colFormLabel" placeholder="Password Here!"/>
@@ -38,7 +46,60 @@ export default function CheckPassword({option}) {
 </form>
   </div>
   <div className="card-footer py-5 text-muted">
-    2 days ago
+    {
+        strengthBar === '0' &&
+        <div className="progress">
+  <div className="progress-bar progress-bar-striped bg-dark" role="progressbar" aria-label="Dark striped example" style={{width: '2%'}} aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
+    }
+
+{
+        strengthBar === '1' &&
+        <div className="progress">
+  <div className="progress-bar progress-bar-striped bg-secondary" role="progressbar" aria-label="Secondary striped example" style={{width: '10%'}} aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
+    }
+
+{
+        strengthBar === '2' &&
+        <div className="progress">
+  <div className="progress-bar progress-bar-striped bg-danger" role="progressbar" aria-label="Danger striped example" style={{width: '20%'}} aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
+    }
+
+{
+        strengthBar === '3' &&
+        <div className="progress">
+  <div className="progress-bar progress-bar-striped bg-warning" role="progressbar" aria-label="Warning striped example" style={{width: '40%'}} aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
+    }
+
+{
+        strengthBar === '4' &&
+        <div className="progress">
+  <div className="progress-bar progress-bar-striped bg-info" role="progressbar" aria-label="Info striped example" style={{width: '60%'}} aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
+    }
+
+{
+        strengthBar === '5' &&
+        <div className="progress">
+  <div className="progress-bar progress-bar-striped bg-primary" role="progressbar" aria-label="Primary striped example" style={{width: '80%'}} aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
+    }
+    {
+        strengthBar === '6' &&
+        <div className="progress">
+  <div className="progress-bar progress-bar-striped bg-success" role="progressbar" aria-label="Success striped example" style={{width: '100%'}} aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
+    }
   </div>
 </div>
     </div>
