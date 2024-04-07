318c1fe6f84b9ae9b8e44dc7f3f009d8fe15ae6d
alexquar <alexanderquarrie@gmail.com>
2024-04-07 17:45:05 -0400

    check password function

--- src/pages/CheckPassword.jsx
+++ src/pages/CheckPassword.jsx
@@ -1,5 +1,24 @@
-
-export default function CheckPassword() {
+import { useState } from "react"
+export default function CheckPassword({option}) {
+    const [password,setPassword] = useState(option)
+    const [strengthBar, setStrengthBar]= useState('')
+    const checkPassword =()=>{
+        let strength=0;
+        if (password.match(/[a-z]+/)){
+            strength+=1;
+        }
+        if (password.match(/[A-Z]+/)){
+            strength+=1;
+        }
+        if (password.match(/[0-9]+/)){
+            strength+=1;
+        }
+        if (password.match(/[$@#&!*^.'"|-_=+%,<>;;`~?]+/)){
+            strength+=1;
+    
+            }
+        setStrengthBar(`${strength}`)
+    }
   return (
     <div>
       
