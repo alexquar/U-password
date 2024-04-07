3ea96a0615733d7c5bb17e08a12cef85c1b49d8d
alexquar <alexanderquarrie@gmail.com>
2024-04-07 10:34:35 -0400

    signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,36 @@
 import { useState } from "react"
 
 export default function Login() {
-
+  const [password, setPassword]= useState('')
+  const [email,setEmail]= useState('')
   return (
-  <div>
-    
+    <div className="container text-center py-5 my-5">
+    <div className="row justify-content-center">
+    <div class="card col-12 col-sm-10 col-lg-6">
+    <div class="card-body">
+      <form className="d-flex flex-column" >
+        <h2>
+          Login
+        </h2>
+      <label className="my-3" >
+    <span className="card-text mx-2">
+    Email:
+    </span>
+    <input type="email" />
+      </label>
+      <label className="my-3" >
+    <span className="mx-2 card-text">
+    Password:
+    </span>
+    <input type="password" />
+      </label>
+      <div className="d-grid mt-3 col-4 mx-auto">
+      <button className="btn btn-dark">Login</button>
+      </div>
+      </form>
+    </div>
+  </div>
+  </div>
   </div>
   )
 }
