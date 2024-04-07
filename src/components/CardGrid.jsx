e80ac489a4c45752ee40c80b9f491121df683fcc
alexquar <alexanderquarrie@gmail.com>
2024-04-07 15:45:48 -0400

    password card component

---
+++ src/components/CardGrid.jsx
@@ -0,0 +1,9 @@
+
+
+export default function CardGrid() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/components/PasswordCard.jsx
+++ src/components/PasswordCard.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react"
 
-export default function PasswordCard() {
+export default function PasswordCard(service,createdAt, password ) {
+    const [showPassword, setShowPassword]= useState(false)
+    const handelClick= () =>{
+        if(showPassword){
+            setShowPassword(false)
+        } else {
+            setShowPassword(true)
+        }
+    }
   return (
-    <div>
-      
+    <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Password for {service}</h5>
+      <p className="cart-text">Password created/changed: {createdAt}</p>
+    {!showPassword &&  <button className="btn" onClick={handleClick}>Show Password</button>}
+    {showPassword &&  <button className="btn" onClick={handleClick}>Show Password</button>}
+    {showPassword && <p class="card-text">Password: {password}</p>}
     </div>
+  </div>
   )
 }
