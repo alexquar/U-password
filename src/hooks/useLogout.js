f9c2134b5cd7da3a2efeed83cf4be2cb044dbcc3
alexquar <alexanderquarrie@gmail.com>
2024-04-07 21:10:19 -0400

    uselogout

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { projectAuth, projectFirestore } from '../firebase/config'
+import { useAuthContext } from './useAuthContext'
+
+export const useLogout = () => {
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const { dispatch } = useAuthContext()
+  
+  const logout = async () => {
+    setError(null)
+    setIsPending(true)
+
+    try {
+      // update online status
+      const { uid } = projectAuth.currentUser
+      await projectFirestore.collection('user').doc(uid).update({ online: false })
+      
+      // sign the user out
+      await projectAuth.signOut()
+      
+      // dispatch logout action
+      dispatch({ type: 'LOGOUT' })
+
+
+        setIsPending(false)
+        setError(null)
+    } 
+    catch(err) {
+
+        setError(err.message)
+        setIsPending(false)
+
+    }
+  }
+
+  return { logout, error, isPending }
+}
