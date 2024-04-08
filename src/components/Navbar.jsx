f001e805b72bab1b4204db8d38de191a4cee57c0
alexquar <alexanderquarrie@gmail.com>
2024-04-07 21:08:44 -0400

    conditional navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,27 @@
 import {Link} from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
+import { useContext } from 'react'
 export default function Navbar() {
+  const {user}=useAuthContext()
   return (
     <nav class="navbar navbar-dark bg-dark py-3">
   <div class="container-fluid container">
     <Link class="navbar-brand" to='/'> <h1>U Passwords</h1></Link>
     <div class="d-flex">
+      {user &&
       <div>
       <Link className='mx-3 text-light' to='/passwords/saved'>Saved</Link>
       <Link className='mx-3 text-light' to='/passwords/eval'>Check</Link>
       <Link className='mx-3 text-light' to='/passwords/create'>Create</Link>
       <button  className="btn btn-outline-light mx-3">Logout</button>
       </div>
+}
+      {!user &&
       <div>
       <Link  className='mx-3 text-light' to='/signup'>Signup</Link>
       <Link   className='mx-3 text-light' to='/login'> Login </Link>
       </div>
+}
     </div>
   </div>
 </nav>
