c6328ac802579527a47706f7cc1c6f189b7e3d8b
alexquar <alexanderquarrie@gmail.com>
2024-04-11 16:28:51 -0400

    delete password

--- src/components/PasswordCard.jsx
+++ src/components/PasswordCard.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react"
-
+import { useFirestore } from "../hooks/useFirestore"
 export default function PasswordCard({password : data}) {
     const [showPassword, setShowPassword]= useState(false)
-    const {service, password, createdAt} = data
+    const {service, password, createdAt, id} = data
+    const {deleteDocument} = useFirestore('passwords')
     const handleClick= () =>{
         if(showPassword){
             setShowPassword(false)
         } else {
             setShowPassword(true)
         }
+    }
+    const handleDelete = () => {
+      deleteDocument(id)
     }
   return (
     <div className="card text-bg-dark my-3">
@@ -20,6 +24,7 @@ export default function PasswordCard({password : data}) {
     <div>
     {showPassword && <p className=" block badge rounded-pill text-bg-light mt-3 mb-0 p-3">Password: {password}</p>}
     </div>
+    <button onClick={handleDelete}>delete</button>
     </div>
   </div>
   )
