d16965e57fb60543c73b3d0fcf822a81c08c55d9
alexquar <alexanderquarrie@gmail.com>
2024-04-11 20:20:51 -0400

    delete

--- src/components/PasswordCard.jsx
+++ src/components/PasswordCard.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { useFirestore } from "../hooks/useFirestore"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBan } from '@fortawesome/free-solid-svg-icons'
 export default function PasswordCard({password : data}) {
     const [showPassword, setShowPassword]= useState(false)
     const {service, password, createdAt, id} = data
@@ -17,14 +19,16 @@ export default function PasswordCard({password : data}) {
   return (
     <div className="card text-bg-dark my-3">
     <div className="card-body">
-      <h5 className="card-title">Password for {service}</h5>
+      <div className="d-flex">
+      <h5 className="card-title mx-auto">Password for {service}</h5>
+      <FontAwesomeIcon onClick={handleDelete} className='mb-1 pb-4'  size='lg' icon={faBan} style={{color: "#ffffff",}} />
+      </div>
     {createdAt &&  <p className="cart-text">Password created/changed: {createdAt.toDate().toDateString()}</p>}
     {!showPassword &&  <button className="btn btn-outline-light" onClick={handleClick}>Show Password</button>}
     {showPassword &&  <button className="btn btn-outline-light" onClick={handleClick}>Show Password</button>}
     <div>
     {showPassword && <p className=" block badge rounded-pill text-bg-light mt-3 mb-0 p-3">Password: {password}</p>}
     </div>
-    <button onClick={handleDelete}>delete</button>
     </div>
   </div>
   )
